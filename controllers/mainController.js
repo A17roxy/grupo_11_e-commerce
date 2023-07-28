d8452b0cb0d29c339abcfb39b6ab331b6bef4856
fjlozada <fjlozada@hotmail.com>
2023-07-28 00:47:52 -0400

    header y footer en ejs separados e includes en albums y login

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const controller ={
     home: (req,res) => {res.render('home')},
-    login: (req,res) => {res.render('login1')},
+    login: (req,res) => {res.render('login')},
     register: (req,res) => {res.render('register')},
 }
 
