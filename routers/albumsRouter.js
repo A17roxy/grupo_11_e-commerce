4147e28b5dd686e84142d3c1c93ae930ba32dded
fjlozada <fjlozada@hotmail.com>
2023-08-08 21:59:00 -0400

    nos quedo modificado el router de albums

--- routers/albumsRouter.js
+++ routers/albumsRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const albumsController = require('../controllers/albumsController.js');
 
-/* www.url.com/clientes ->  clientes viene el app.use en app.js*/
+/* www.url.com/albums */
 router.get('/',albumsController.albums);
+
 
 module.exports = router ;
