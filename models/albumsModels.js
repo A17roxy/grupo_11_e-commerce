a0bfcf6602b146491f264759d46f5e121c05c1f8
fjlozada <fjlozada@hotmail.com>
2023-08-08 21:39:42 -0400

    Ordenamos carpetas

--- models/albumsModels.js
+++ models/albumsModels.js
@@ -3,13 +3,11 @@ const path = require('path');
 
 const model = {
     findAll: () => {
-        const jsonData = fs.readFileSync(path.join(__dirname, '../data/users.json'));
+        const jsonData = fs.readFileSync(path.join(__dirname, '../data/products.json'));
     },
 
     findById: (id) => {
-
     }
 }
-
 
 module.exports = model;
