a0c1d29a020545ef308dea3ab05ad3ad684d360f
Analie <analie@localhost>
2023-08-11 18:12:50 -0300

    Register c/categorias comentadas

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
+// @GET /products
 router.get('/users', usersController.getList);
 
 module.exports = router;
