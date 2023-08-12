48e6b8739b15c1783b144e5dee2fe21f138be5f1
Analie <analie@localhost>
2023-08-12 14:49:54 -0300

    /users

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
-// @GET /products
+// @GET /users
 router.get('/users', usersController.getList);
 
 module.exports = router;
