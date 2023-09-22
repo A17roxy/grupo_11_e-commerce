597be708e6e9520c3156e31b28785bc2b97cdd57
fjlozada <fjlozada@hotmail.com>
2023-09-21 21:57:12 -0400

    dejamos rutas de pruebas para agustin/matias/fran/ana

--- controllers/pruebasController.js
+++ controllers/pruebasController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const controller ={
-    home: async (req,res) => {
+    agustin: async (req,res) => {
         /* res.sendfile(path.resolve(__dirname,'../views/home.html')); */
         const db=require('../database/models')
         try {
@@ -28,7 +28,87 @@ const controller ={
         
     },
 
-    
+    matias: async (req,res) => {
+        /* res.sendfile(path.resolve(__dirname,'../views/home.html')); */
+        const db=require('../database/models')
+        try {
+            // EJEMPLO DE FINDBYPK y SUS RETORNOS
+            // const resultado = await db.generos.findByPk(1)
+            // console.log(resultado.dataValues)
+            // console.log(resultado.dataValues.nombre)
+            // EJEMPLO DE FINDALL Y SUS RETORNOS
+             const resultado = await db.users.findAll();
+             //{where: {nombre: 'Inject The Venom'} en los parentesis del findAll para filtrar
+             //{order: ['nombre']}
+             // este mapea los datos del registro entero, todas las filas
+             const genx=resultado.map(elem=>elem.dataValues);
+             // este mapea el campo nombre, de todos los registros (map de map)
+             // const prueba=respuesta.map(x=>x.dataValues).map(x=>x.nombre);
+             console.log(genx);
+             res.send(genx);
+             //res.send(genx);
+             //console.log(resultado.dataValues.nombre)      
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+        
+    },
+
+    fran: async (req,res) => {
+        /* res.sendfile(path.resolve(__dirname,'../views/home.html')); */
+        const db=require('../database/models')
+        try {
+            // EJEMPLO DE FINDBYPK y SUS RETORNOS
+            // const resultado = await db.generos.findByPk(1)
+            // console.log(resultado.dataValues)
+            // console.log(resultado.dataValues.nombre)
+            // EJEMPLO DE FINDALL Y SUS RETORNOS
+             const resultado = await db.users.findAll();
+             //{where: {nombre: 'Inject The Venom'} en los parentesis del findAll para filtrar
+             //{order: ['nombre']}
+             // este mapea los datos del registro entero, todas las filas
+             const genx=resultado.map(elem=>elem.dataValues);
+             // este mapea el campo nombre, de todos los registros (map de map)
+             // const prueba=respuesta.map(x=>x.dataValues).map(x=>x.nombre);
+             console.log(genx);
+             res.send(genx);
+             //res.send(genx);
+             //console.log(resultado.dataValues.nombre)      
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+        
+    },
+
+
+    ana: async (req,res) => {
+        /* res.sendfile(path.resolve(__dirname,'../views/home.html')); */
+        const db=require('../database/models')
+        try {
+            // EJEMPLO DE FINDBYPK y SUS RETORNOS
+            // const resultado = await db.generos.findByPk(1)
+            // console.log(resultado.dataValues)
+            // console.log(resultado.dataValues.nombre)
+            // EJEMPLO DE FINDALL Y SUS RETORNOS
+             const resultado = await db.users.findAll();
+             //{where: {nombre: 'Inject The Venom'} en los parentesis del findAll para filtrar
+             //{order: ['nombre']}
+             // este mapea los datos del registro entero, todas las filas
+             const genx=resultado.map(elem=>elem.dataValues);
+             // este mapea el campo nombre, de todos los registros (map de map)
+             // const prueba=respuesta.map(x=>x.dataValues).map(x=>x.nombre);
+             console.log(genx);
+             res.send(genx);
+             //res.send(genx);
+             //console.log(resultado.dataValues.nombre)      
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+        
+    },
 
 }
 
--- routers/pruebasRouter.js
+++ routers/pruebasRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const pruebasController = require('../controllers/pruebasController.js');
 
 /* www.url.com/pruebas ->  clientes viene el app.use en app.js*/
-router.get('/',pruebasController.home);
+router.get('/matias',pruebasController.matias);
+router.get('/agustin',pruebasController.agustin);
+router.get('/fran',pruebasController.fran);
+router.get('/ana',pruebasController.ana);
 
 module.exports = router ;
